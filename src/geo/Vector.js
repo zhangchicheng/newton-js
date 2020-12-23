a8890cdd846c584f6ddd7b3c46a877106140e605
chicheng <zhang.chic@northeastern.edu>
2020-12-22 22:24:53 -0800

    add vector function

--- src/geo/Vector.js
+++ src/geo/Vector.js
@@ -1,5 +1,11 @@
 var Vector = {};
 
 (function() {
+    Vector.dot = function(v1, v2) {
+        return (v1.x * v2.x) + (v1.y * v2.y);
+    }
 
+    Vector.cross = function(v1, v2) {
+        return (v1.x * v2.y) - (v1.y * v2.x);
+    };
 })();
