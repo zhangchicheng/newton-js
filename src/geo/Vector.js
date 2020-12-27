dd8c326471142b83d78990256b91fae6c9fee0d0
chicheng <zhang.chic@northeastern.edu>
2020-12-26 23:00:47 -0800

    modify Vector.js

--- src/geo/Vector.js
+++ src/geo/Vector.js
@@ -8,4 +8,19 @@ var Vector = {};
     Vector.cross = function(v1, v2) {
         return (v1.x * v2.y) - (v1.y * v2.x);
     };
+
+    Vector.cross3 = function(v1, v2, v3) {
+        return (v2.x - v1.x) * (v3.y - v1.y) - (v2.y - v1.y) * (v3.x - v1.x);
+    }
+
+    Vector.magnitude = function(v) {
+        return Math.sqrt((v.x * v.x) + (v.y * v.y));
+    }
+
+    Vector.normalize = function(v) {
+        let magnitude = Vector.magnitude(v);
+        if (magnitude === 0)
+            return { x: 0, y: 0 };
+        return { x: v.x / magnitude, y: v.y / magnitude };
+    }
 })();
